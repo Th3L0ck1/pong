16272f51d71b2f934c78f93cebe82ce8bfd791a4
NHardt <nicollashardturnau@gmail.com>
2026-08-24 21:23:26 -0300

    Add mecanics
    Map colision in y, bouncing ball

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
-var speed = 500
+var speed = 350
 var dir = Vector2(0,0)
 
+@export var up = KEY_W
+@export var down = KEY_S
+@export var right = KEY_D
+@export var left = KEY_A
 
 func _ready() -> void:
 	
@@ -17,13 +21,13 @@ func move(delta):
 	
 	dir = Vector2()
 	
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(right):
 		dir += Vector2(1,0)
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(left):
 		dir += Vector2(-1,0)
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_key_pressed(up) and global_position.y > 0:
 		dir += Vector2(0,-1)
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(down)and global_position.y < get_viewport_rect().size.y:
 		dir += Vector2(0,1)
 	
 	global_position += dir.normalized() * speed * delta
---
+++ scripts/ball.gd
@@ -0,0 +1,43 @@
+extends RigidBody2D
+
+var speed = 450
+var dir = [-1,1]
+var scoreP1 = 0
+var scoreP2 = 0
+
+func _ready() -> void:
+	
+	reset()
+	
+	pass
+
+
+
+func _physics_process(delta: float) -> void:
+	
+	score()
+	pass
+	
+func reset():
+	global_position = get_viewport_rect().size / 2
+	
+	freeze = true
+	await get_tree().create_timer(2).timeout
+	freeze = false
+	
+	apply_central_impulse(Vector2( dir.pick_random() * speed ,dir.pick_random() * speed ))
+	
+	
+	pass
+	
+func score():
+	
+	if global_position.x >= get_viewport_rect().size.x:
+		reset()
+		scoreP1 += 1
+	if global_position.x <= 0:
+		reset()
+		scoreP2 += 1
+	
+	print(scoreP1 , ":" , scoreP2)
+	pass
